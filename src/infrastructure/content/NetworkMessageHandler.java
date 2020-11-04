330f49496a2bf9417fcba9f7ef0973cae193ac25
Talha Yaseen <khantalhakhan94@gmail.com>
2020-11-04 23:28:31 +0530

    onMessageReceived() method updated

--- src/infrastructure/content/NetworkMessageHandler.java
+++ src/infrastructure/content/NetworkMessageHandler.java
@@ -1,4 +1,5 @@
 import networking.INotificationHandler;
+import org.json.*; 
 
 /*
  * NetworkMessageHandler class includes one method i.e @onMessageReceived()
@@ -8,11 +9,24 @@ public class NetworkMessageHandler implements INotificationHandler {
 	/*
 	 * @param: json string message (Meta fields are newUser or message or userExit)
 	 * if meta field is newUser, then remaining field would only be username
-	 * if meta field is message, then remaining fields would be username, message and time
+	 * if meta field is actualMsg, then remaining fields would be username, msg and time
 	 * if meta field is exitUser, then remaining field would only be username
 	 * Networking module will be calling this method with json string message to send data to the content module
 	 */
 	public void onMessageReceived(String message) {
+		JSONObject message = new JSONObject(message);
+		String type = message.getString("type");
 		
+		if(type.equals("newUser")) {
+			String username = message.getString("username");
+		}
+		else if(type.equals("actualMsg")) {
+			String username = message.getString("username");
+			String msg = message.getString("msg");
+			String time = message.getString("time");
+		}
+		else {
+			String username = message.getString("username");
+		}
 	}
 }

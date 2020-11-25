07bfabcacdccf9e0fcf197d783f59dea620a47aa
Rahul R <r.rahul.ug@gmail.com>
2020-11-25 05:40:33 +0530

    updated driver code

--- src/infrastructure/validation/testing/driver.java
+++ src/infrastructure/validation/testing/driver.java
@@ -1,10 +1,11 @@
 package infrastructure.validation.testing;
 
 import infrastructure.validation.testing.TestHarness;
-class runner{
+import infrastructure.tests.HarnessInstanceTest;
+class driver{
 	public static void main(String args[]){
 		TestHarness th = new TestHarness();
-		String testNamePath = "src/infrastructure/tests/HarnessTest.java";
+		String testNamePath = "src/infrastructure/tests/HarnessInstanceTest";
 		th.runByName(testNamePath);
 
 	}

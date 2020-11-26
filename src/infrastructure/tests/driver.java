8352f0ca8a362a34039a4c1b6453e31d3709142d
Rahul R <r.rahul.ug@gmail.com>
2020-11-26 08:15:27 +0530

    updated driver to run ConsoleLoggerTest

--- src/infrastructure/tests/driver.java
+++ src/infrastructure/tests/driver.java
@@ -1,6 +1,7 @@
 package infrastructure.tests;
 import infrastructure.tests.LoggerInstanceTest;
-import infrastructure.validation.testing.ITest;
+import infrastructure.tests.ConsoleLoggerTest;
+//import infrastructure.validation.testing.ITest;
 import infrastructure.validation.testing.TestCase;
 /**
  * Replaced with test harness when that is working
@@ -9,7 +10,7 @@ import infrastructure.validation.testing.TestCase;
  */
 class driver{
 	public static void main(String args[]) {
-		TestCase test = new LoggerInstanceTest();
+		TestCase test = new ConsoleLoggerTest();
 		boolean result = test.run();
 		System.out.println(result);
 	}
